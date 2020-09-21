0eda42a944fb51cdfc9ba827e92c30c70002b00f
Matheus Fidelis <matheus@nanoshots.com.br>
2020-09-20 23:19:02 -0300

    funcionando essa merda

--- app_consumer_log/go.mod
+++ app_consumer_log/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/confluentinc/confluent-kafka-go v1.4.2 // indirect
+	github.com/segmentio/kafka-go v0.4.2
 	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.4.2
 )
--- app_producer/main.go
+++ app_producer/main.go
@@ -12,15 +12,36 @@ import (
 )
 
 func main() {
+
 	topic := os.Getenv("KAFKA_TOPIC")
 	boostrap_servers := os.Getenv("KAFKA_BROKER")
 
 	writer := getWriter(boostrap_servers, topic)
 	defer writer.Close()
 
+	// Async Producer
+	// for {
+	// 	r := make(chan int32)
+
+	// 	// Async Producer
+	// 	go func() {
+
+	// 		msg := kafka.Message{
+	// 			Key:   []byte(guuid.New().String()),
+	// 			Value: []byte(guuid.New().String()),
+	// 		}
+
+	// 		err := writer.WriteMessages(context.Background(), msg)
+
+	// 		if err != nil {
+	// 			fmt.Println(err)
+	// 		}
+	// 		r <- 1
+	// 	}()
+	// }
+
+	// Sync Producer
 	for {
-		// fmt.Println(topic)
-		// fmt.Println(boostrap_servers)
 
 		msg := kafka.Message{
 			Key:   []byte(guuid.New().String()),
@@ -34,22 +55,32 @@ func main() {
 		}
 
 		time.Sleep(time.Second)
+
 	}
+
 }
 
 func getWriter(bootstrap_servers, topic string) *kafka.Writer {
 
+	name, err := os.Hostname()
+
+	if err != nil {
+		panic(err)
+	}
+
 	dialer := &kafka.Dialer{
 		Timeout:  10 * time.Second,
-		ClientID: "oi",
+		ClientID: name,
 	}
 
 	return kafka.NewWriter(kafka.WriterConfig{
-		Brokers:      strings.Split(bootstrap_servers, ","),
-		Topic:        "demo",
-		Balancer:     &kafka.LeastBytes{},
+		Brokers: strings.Split(bootstrap_servers, ","),
+		Topic:   topic,
+		// Balancer:     &kafka.LeastBytes{},
+		Balancer:     &kafka.Hash{},
 		Dialer:       dialer,
-		WriteTimeout: 10 * time.Second,
-		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  5 * time.Second,
 	})
+
 }
